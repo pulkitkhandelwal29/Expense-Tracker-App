afe4fae42641dc120c15f87d4ba1a3def55e4678
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-05-13 14:41:57 +0530

    Added images to app

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,60 +13,76 @@ class TransactionList extends StatelessWidget {
     return Container(
       height: 300,
       //ListView - column + SingleChildScrollView
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  //styling
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      //Accessing theme of app
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  //double needs to be converted to string for Text
-                  child: Text(
-                    //string interpolation with 2 decimal places for amount
-                    '\u{20B9} ${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text('No transactions added yet!',
+                    style: Theme.of(context).textTheme.titleMedium),
+                SizedBox(
+                  height: 20,
                 ),
-                Column(
-                  //will make left alignment
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    Text(
-                      //Date Formatting from intl package
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                )
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ))
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        //styling
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            //Accessing theme of app
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        //double needs to be converted to string for Text
+                        child: Text(
+                          //string interpolation with 2 decimal places for amount
+                          '\u{20B9} ${transactions[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        //will make left alignment
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transactions[index].title,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          Text(
+                            //Date Formatting from intl package
+                            DateFormat.yMMMd().format(transactions[index].date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              //run listview till the length of transactions
+              itemCount: transactions.length,
             ),
-          );
-        },
-        //run listview till the length of transactions
-        itemCount: transactions.length,
-      ),
     );
   }
 }
